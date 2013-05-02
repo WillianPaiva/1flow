064b401a6e88c46c6884ed6c2d2d6067bff1ed4a
Olivier Cortès <olive@licorn.org>
2013-05-03 00:16:46 +0200

    Activate admin site for all apps, finally.

--- oneflow/urls.py
+++ oneflow/urls.py
@@ -2,6 +2,10 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib import admin
+
+admin.autodiscover()
+
 
 if settings.SITE_ID == 1:
     urlpatterns = patterns(
@@ -10,13 +14,9 @@ if settings.SITE_ID == 1:
         # NEVER use r'^$', this won't work as expected. Use r''.
         url(r'', include('oneflow.landing.urls')),
 
-        # No admin on the landing page site.
     )
 
 else:
-    from django.contrib import admin
-    admin.autodiscover()
-
     urlpatterns = patterns(
         '',
 
@@ -25,11 +25,14 @@ else:
 
         # url(r'^api/', include('oneflow.api.urls')),
 
-        url(r'^grappelli/', include('grappelli.urls')),
-        url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-        url(r'^admin/', include(admin.site.urls)),
-
     )
+
+urlpatterns += patterns(
+    '',
+    url(r'^grappelli/', include('grappelli.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
 
 # This will add urls only when DEBUG=True
 # cf. https://docs.djangoproject.com/en/1.5/ref/contrib/staticfiles/
