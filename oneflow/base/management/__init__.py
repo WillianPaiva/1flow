98d4a2b8f2be58ef29f2b25a5a02da24a2578bd0
Olivier Cortès <olive@licorn.org>
2013-05-03 01:50:16 +0200

    polish.

--- oneflow/base/management/__init__.py
+++ oneflow/base/management/__init__.py
@@ -1,22 +1,36 @@
 # -*- coding: utf-8 -*-
 
+import logging
+
 from django.contrib.sites import models
 from django.db.models import signals
 from django.conf import settings
 
 
+LOGGER = logging.getLogger(__name__)
+
+
 def create_site(app, created_models, verbosity, **kwargs):
     """ Create the default site when when we install the sites framework. """
 
-    if not models.Site in created_models:
-        return
-
     try:
-        models.Site.objects.get(pk=settings.SITE_ID)
+        site = models.Site.objects.get(pk=settings.SITE_ID)
 
     except models.Site.DoesNotExist:
-        models.Site.objects.create(pk=settings.SITE_ID,
-                                   name=settings.SITE_NAME,
-                                   domain=settings.SITE_DOMAIN).save()
+        site = models.Site.objects.create(pk=settings.SITE_ID,
+                                          name=settings.SITE_NAME,
+                                          domain=settings.SITE_DOMAIN)
+        site.save()
+
+        LOGGER.debug('Site created: %s', site)
+
+    else:
+        # update Django's example.com with our real data.
+        site.name   = settings.SITE_NAME
+        site.domain = settings.SITE_DOMAIN
+        site.save()
+
+        LOGGER.debug('Site updated: %s', site)
+
 
 signals.post_syncdb.connect(create_site, sender=models)
