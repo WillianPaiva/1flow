4924b3b168a04b8ebdac8cf88dacb84bf548aa65
Olivier Cortès <olive@licorn.org>
2013-05-03 22:36:28 +0200

    countdown inclusion template tag.

--- oneflow/base/templatetags/base_utils.py
+++ oneflow/base/templatetags/base_utils.py
@@ -3,6 +3,9 @@
 from django import template
 from django.template.base import Node, TemplateSyntaxError
 from django.utils.encoding import smart_text
+from django.utils.translation import ugettext_lazy as _
+
+from sparks.foundations import utils as sfu
 
 register = template.Library()
 
@@ -45,3 +48,54 @@ def firstofas(parser, token):
 
     return FirstOfAsNode([parser.compile_filter(bit) for bit in bits],
                          variable_name)
+
+
+@register.inclusion_tag('snippets/countdown.html')
+def countdown(value, redirect=None, limit=0, show_seconds=True, short=False):
+    """ From http://www.plus2net.com/javascript_tutorial/countdown.php """
+
+    if redirect is None:
+        redirect = '/'
+
+    if limit > 0:
+        operation    = '+'
+        round_value  = 0
+        counter_test = '<='
+
+    else:
+        operation    = '-'
+        round_value  = 0        # WAS: 2
+        counter_test = '>='
+
+    return {
+        'name': sfu.unique_hash(only_letters=True),
+        'short': short,
+        'value': value,
+        'limit': limit,
+        'redirect': redirect,
+        'operation': operation,
+        'round_value': round_value,
+        'show_seconds': show_seconds,
+        'counter_test': counter_test,
+        'separator': ' ' if short else ', ',
+        'units': {
+            'day': _('d'),
+            'days': _('d'),
+            'hour': _('h'),
+            'hours': _('h'),
+            'minute': _('m'),
+            'minutes': _('m'),
+            'second': _('s'),
+            'seconds': _('s'),
+        } if short else {
+            # NOTE: the starting spaces are intentional.
+            'day': _(' day'),
+            'days': _(' days'),
+            'hour': _(' hour'),
+            'hours': _(' hours'),
+            'minute': _(' minute'),
+            'minutes': _(' minutes'),
+            'second': _(' second'),
+            'seconds': _(' seconds'),
+        },
+    }
