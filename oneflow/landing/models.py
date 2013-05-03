a2ac685a84e5bab716aba0bfa0e064e01b0bdc2e
Olivier Cortès <olive@licorn.org>
2013-05-03 14:41:24 +0200

    landing models and views updates for transmeta DB contents.

--- oneflow/landing/models.py
+++ oneflow/landing/models.py
@@ -1,6 +1,8 @@
+# -*- coding: utf-8 -*-
 
 from transmeta import TransMeta
 
+from django.utils.translation import ugettext_lazy as _
 from django.db import models
 
 
@@ -9,6 +11,11 @@ class LandingContent(models.Model):
 
     name    = models.CharField(max_length=128)
     content = models.TextField()
+
+    def __unicode__(self):
+        return _(u'{field_name}: {truncated_field_value}').format(
+            field_name=self.name, truncated_field_value=self.content[:30]
+            + (self.content[30:] and u'…'))
 
     class Meta:
         translate = ('content', )
--- oneflow/landing/views.py
+++ oneflow/landing/views.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
+import logging
 
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.shortcuts import render
-
+from django.utils.translation import get_language
 from forms import LandingPageForm
+from models import LandingContent
+
+LOGGER = logging.getLogger(__name__)
 
 
 def home(request):
@@ -33,6 +37,11 @@ def home(request):
         form = LandingPageForm()
 
     context['form'] = form
+
+    lang = get_language().split('_', 1)[0]
+
+    contents = LandingContent.objects.values_list('name', 'content_' + lang)
+    context.update(contents)
 
     return render(request, 'landing_index.html', context)
 
