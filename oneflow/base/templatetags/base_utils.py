8b65bfe606a782f1b4b2002a79f432ee344f6ede
Olivier Cortès <olive@licorn.org>
2013-05-06 09:50:59 +0200

    Introduce the `captureas` wonderful template tag.

--- oneflow/base/templatetags/base_utils.py
+++ oneflow/base/templatetags/base_utils.py
@@ -10,6 +10,17 @@ from sparks.foundations import utils as sfu
 register = template.Library()
 
 
+class CaptureasNode(template.Node):
+    def __init__(self, nodelist, varname):
+        self.nodelist = nodelist
+        self.varname  = varname
+
+    def render(self, context):
+        output = self.nodelist.render(context)
+        context[self.varname] = output
+        return ''
+
+
 class FirstOfAsNode(Node):
     def __init__(self, args, variable_name=None):
         self.vars = args
@@ -28,6 +39,25 @@ class FirstOfAsNode(Node):
                     return smart_text(value)
 
         return ''
+
+
+@register.tag(name='captureas')
+def do_captureas(parser, token):
+    """ Taken from http://djangosnippets.org/snippets/545/ verbatim. Handy!
+        Initial source: https://code.djangoproject.com/ticket/7239
+    """
+
+    try:
+        tag_name, args = token.contents.split(None, 1)
+
+    except ValueError:
+        raise template.TemplateSyntaxError(
+            "'captureas' node requires a variable name.")
+
+    nodelist = parser.parse(('endcaptureas',))
+    parser.delete_first_token()
+
+    return CaptureasNode(nodelist, args)
 
 
 @register.tag
