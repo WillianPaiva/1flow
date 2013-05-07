29168bcfdce8058a8dbb850e98b2960fcad5f5d7
Olivier Cortès <olive@licorn.org>
2013-05-07 08:02:56 +0200

    Simplify urls.py.

--- oneflow/landing/urls.py
+++ oneflow/landing/urls.py
@@ -2,11 +2,9 @@
 from django.conf.urls import patterns, url
 from django.utils.translation import ugettext_lazy as _
 
-import views
-
 urlpatterns = patterns(
-    'oneflow.landing',
-    url(r'^$', views.home, name='landing_home'),
-    url(_(r'^thanks/(?P<already_registered>\w+)?$'), views.thanks,
-        name='landing_thanks'),
+    'oneflow.landing.views',
+    url(r'^$', 'home', name='landing_home'),
+    url(_(r'^thanks/(?P<already_registered>\w+)?$'),
+        'thanks', name='landing_thanks'),
 )
