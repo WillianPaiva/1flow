c5d03a3e63c476c87b93c1f2a95b55506d3e7460
Olivier Cortès <olive@licorn.org>
2013-05-08 19:16:59 +0200

    Tooltip the lang flags.

--- oneflow/landing/static/js/script.js
+++ oneflow/landing/static/js/script.js
@@ -6,4 +6,8 @@ $(document).ready(function() {
     $('.feature a').tooltip();
     $('.header-feature a').tooltip();
     $('a.link-tooltip').tooltip();
+
+    try {
+        $("#lang-flags input[type=image]").tooltip({placement: 'bottom'});
+    } catch(err) {};
 });
