8c32a571769612c1863306ad9c4ffc55ebb2622a
Olivier Cortès <olive@licorn.org>
2013-05-09 01:49:30 +0200

    settings/*/cache*: move production cache to 127.0.0.1 to avoid messing with many django apps in the same memcached.

--- oneflow/settings/snippets/cache_common.py
+++ oneflow/settings/snippets/cache_common.py
@@ -2,6 +2,7 @@
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
+        'LOCATION': '127.0.0.1:11211'
     }
 }
 
--- oneflow/settings/snippets/cache_production.py
+++ oneflow/settings/snippets/cache_production.py
@@ -1,4 +1,2 @@
 
-CACHES['default']['LOCATION'] = '10.0.3.1:11211'
-
 REDIS_DB = 0
