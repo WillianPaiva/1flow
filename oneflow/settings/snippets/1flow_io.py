105f2f002879192d35ac96db676c253949ed38c6
Olivier Cortès <olive@licorn.org>
2013-05-10 12:21:29 +0200

    Migrate 1flow{app.com,.net} to 1flow.io only (one site instead of two).

--- oneflow/settings/snippets/1flow_io.py
+++ oneflow/settings/snippets/1flow_io.py
@@ -2,8 +2,8 @@
 import datetime
 
 SITE_ID     = 1
-SITE_DOMAIN = '1flowapp.com'
-SITE_NAME   = '1flow App'
+SITE_DOMAIN = '1flow.io'
+SITE_NAME   = '1flow'
 
 
 LANDING_BETA_DATE = datetime.datetime(2013, 06, 15)
--- oneflow/settings/snippets/1flow_net.py
+++ oneflow/settings/snippets/1flow_net.py
@@ -1,4 +1,11 @@
 
+#
+# As of 20130510, this file is not used anymore, all domains
+# (1flow.net and 1flowapp.com) have been merged into 1flow.io.
+# Kept here for memory of configuration where we had 2 sites
+# on the same django source code.
+#
+
 SITE_ID     = 2
 SITE_DOMAIN = '1flow.net'
 SITE_NAME   = '1flow'
