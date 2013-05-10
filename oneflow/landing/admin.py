45e86667311f4c9b79d90a3f86e71ffc072b1219
Olivier Cortès <olive@licorn.org>
2013-05-10 14:59:14 +0200

    Move the `truncate_field` pseudo-decorator to sparks (which just released 1.17).

--- oneflow/landing/admin.py
+++ oneflow/landing/admin.py
@@ -3,8 +3,7 @@
 from django.contrib import admin
 from django.conf import settings
 from .models import LandingContent
-
-TRUNCATE_LENGTH = 50
+from sparks.django.admin import truncate_field
 
 content_fields_names = tuple(('content_' + code)
                              for code, lang
@@ -14,29 +13,20 @@ content_fields_displays = tuple((field + '_display')
 
 
 class LandingContentAdmin(admin.ModelAdmin):
-    list_display = ('name', ) + content_fields_displays
+    #
     #list_display_links = ('name')
-
     #list_filter = (HasTranslationFilter(lang)
     #               for lang, lang_name in settings.LANGUAGES)
+    #
+    list_display = ('name', ) + content_fields_displays
     ordering = ('name',)
     search_fields = ('name', ) + content_fields_names
 
 
-def truncated(cls, field_name):
-    def wrapped(self, obj):
-        value = getattr(obj, field_name)
-        return value[:TRUNCATE_LENGTH] + (value[TRUNCATE_LENGTH:] and u'…')
-
-    wrapped.short_description = cls._meta.get_field_by_name(
-        field_name)[0].verbose_name
-    wrapped.admin_order_field = field_name
-    return wrapped
-
 for attr, attr_name in zip(content_fields_names,
                            content_fields_displays):
     setattr(LandingContentAdmin, attr_name,
-            truncated(LandingContent, attr))
+            truncate_field(LandingContent, attr))
 
 
 admin.site.register(LandingContent, LandingContentAdmin)
