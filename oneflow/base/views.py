ce892d00e20dec1b38e62abe13af56dec9586a8c
Olivier Cortès <olive@licorn.org>
2013-05-12 21:08:39 +0200

    Our 404 custom handler, cleanups and comments.

--- oneflow/base/views.py
+++ oneflow/base/views.py
@@ -3,28 +3,41 @@
 import logging
 
 #from django.utils.translation import ugettext_lazy as _
+from django.http import HttpResponseNotFound
 from django.template import RequestContext
 from django.template.loader import render_to_string
-
 from sparks.django.utils import HttpResponseTemporaryServerError
 
 LOGGER = logging.getLogger(__name__)
 
 
-# WTF? @requires_csrf_token
 def error_handler(request, *args, **kwargs):
-    """ Returning a 503 seems much more correct: the situation is clearly
-        temporary, the error will certainly be resolved. For searchbots this
-        seems better too.
+    """ Our error handler returns a 503 instead of a bare 500. Returning a 503
+        seems much more correct: the situation is clearly temporary and the
+        error will certainly be resolved. For search robots this is better too.
 
-        http://agopian.info/blog/django-et-le-handler500-retourner-une-erreur-503.html # NOQA
+        Besides that point, our handler passes the whole request to the 500
+        template, via the whole template context processors chain, which could
+        be considered risky, but is really not because we don't have any
+        custom processor and Django's one are fully tested (at least we hope).
     """
-
-    #if kwargs.get('raise_exception', True):
-    #    LOGGER.exception('Error while processing request')
 
     return HttpResponseTemporaryServerError(render_to_string('500.html',
                                             RequestContext(request)))
+
+
+def not_found_handler(request, *args, **kwargs):
+    """ Our 404 custom handler.
+
+        Basically, it's just a one liner which does exactly what the Django's
+        one does, but our 404.html lies in base/templates instead of
+        ${root}/templates, and we have no way to specify only the modified
+        path. And I don't want to create ${root}/templates just to hold the
+        400.html.
+    """
+
+    return HttpResponseNotFound(render_to_string('404.html',
+                                RequestContext(request)))
 
 
 def crash(request, **kwargs):
