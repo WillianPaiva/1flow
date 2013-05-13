a427bc5833a551c1e886d8e511b666ef6a93873e
Olivier Cortès <olive@licorn.org>
2013-05-13 11:40:33 +0200

    Re-activate the memcache for production. Has it been active in the past??

--- oneflow/settings/obi_1flow_io.py
+++ oneflow/settings/obi_1flow_io.py
@@ -7,20 +7,19 @@ from sparks.django.settings import include_snippets
 include_snippets(
     os.path.dirname(__file__), (
         # no debug on OBI, we need to be in "real-life-mode".
-        # Sentry will help us to get to the point, anyway.
+        # Sentry will help us catch errors, anyway.
         '00_production',
         '1flow_io',
         'common',
         'db_common',
         'db_test',
         'cache_common',
-        # No cache, though, in case we hit database-translations.
         'cache_development',
         'mail_production',
         # But it's a preview/test environment, still.
         'raven_test',
         'common_test',
-        # Thus we get rosetta, and eventually the Django Debug toolbar.
+        # Thus we get rosetta, and the Django Debug toolbar.
         'rosetta',
         'djdt',
     ),
--- oneflow/settings/snippets/cache_1flow_io.py
+++
@@ -1,7 +0,0 @@
-
-# The 1flowapp.com app is fully / globaly cached.
-MIDDLEWARE_CLASSES = (
-    'django.middleware.cache.UpdateCacheMiddleware',
-) + MIDDLEWARE_CLASSES + (
-    'django.middleware.cache.FetchFromCacheMiddleware',
-)
