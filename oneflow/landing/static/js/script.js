f77b0ec4c0cbba63634c5dbe970d5066ebee27c5
Olivier Cortès <olive@licorn.org>
2013-05-15 18:07:01 +0200

    Add new CSS styles and IDs + popover for the inline google presentation.

--- oneflow/landing/static/js/script.js
+++ oneflow/landing/static/js/script.js
@@ -4,10 +4,21 @@
 $(document).ready(function() {
 
     $('.feature a').tooltip();
-    $('.header-feature a').tooltip();
+    $('#promise_right a').tooltip();
     $('a.link-tooltip').tooltip();
 
     try {
         $("#lang-flags input[type=image]").tooltip({placement: 'bottom'});
     } catch(err) {};
+
+
+    $('#promise_left a').popover({
+        trigger: 'hover',
+        template: '<div class="popover"><div class="arrow"></div>'
+        + '<div class="popover-inner popover-presentation">'
+        + '<h3 class="popover-title"></h3><div class="popover-content">'
+        + '<iframe src="https://docs.google.com/presentation/d/1qzrhcuhIfYaVxJmYTFaKANFCJdvZ4zLqcVLiIDk63qw/embed?start=true&loop=false&delayms=3000" '
+        + 'frameborder="0" width="655" height="420" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>'
+        + '</div></div></div>',
+    });
 });
