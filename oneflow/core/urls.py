2f17563be0a659f9a8638d7c030f87772cb89ac8
Olivier Cortès <olive@licorn.org>
2013-05-24 16:51:28 +0200

    Signin/Signout basic process (views, urls…).

--- oneflow/core/urls.py
+++ oneflow/core/urls.py
@@ -1,8 +1,19 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, url
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.decorators import login_required
+
+from .views import home
 
 urlpatterns = patterns(
     'oneflow.core.views',
-    url(_(r'^home/$'), 'home', name='home'),
+    url(_(r'^home/$'), login_required(home), name='home'),
+)
+
+urlpatterns += patterns(
+    'django.contrib.auth.views',
+    url(_(r'^signin/$'), 'login',
+        {'template_name': 'signin.html'}, name='signin'),
+    url(_(r'^signout/$'), 'logout',
+        {'template_name': 'signout.html'}, name='signout'),
 )
