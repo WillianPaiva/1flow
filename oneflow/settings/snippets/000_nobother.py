8f69e695fb0ed7a10d9df5628990c57079025e27
Olivier Cortès <olive@licorn.org>
2013-05-26 11:01:39 +0200

    Add new deprecation warning protection, because more of our external projects use 1.5-deprecated features. Is this an alert??

---
+++ oneflow/settings/snippets/000_nobother.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import warnings
+
+with warnings.catch_warnings():
+    # These warnings are produced by external packages
+    # that we use in 1flow. We have no time to fix them
+    # ourselves, and their developers didn't update their
+    # own code. BUMMER!
+
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+    import django.conf.urls.defaults # NOQA
+    import django.utils.hashcompat # NOQA
