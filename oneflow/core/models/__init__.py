f644ebe2149a9e519b9633f9327aea8edd3263fa
Olivier Cortès <olive@licorn.org>
2013-05-27 16:33:43 +0200

    clean obsolete code.

--- oneflow/core/models/__init__.py
+++ oneflow/core/models/__init__.py
@@ -1,16 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from django.contrib.auth.models import AbstractBaseUser
-
 from .reldb import * # NOQA
 from .nonrel import * # NOQA
 from .keyval import * # NOQA
-
-
-class SimpleUser(AbstractBaseUser):
-    # No need of username, we have the email…
-    #username = models.CharField(max_length=254, unique=True, db_index=True)
-
-    email = models.EmailField(max_length=254, unique=True, db_index=True)
-
-    USERNAME_FIELD = 'email'
