69b1d5120cd5350df1cb9c2f5f93ff8712d20f3d
Olivier Cortès <olive@licorn.org>
2013-05-27 14:49:36 +0200

    Definitive fix the the landing tests. I would really have like to be able to set the language via post/cookie, but perhaps I will find the solution later.

--- oneflow/landing/tests.py
+++ oneflow/landing/tests.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=E1103,C0103
 
-import time
 import logging
 
-from django.test import TestCase
+from django.core import mail
+#from django.conf import settings
 from django.test import TransactionTestCase
 from django.test.client import Client
 from django.test.utils import override_settings
 from django.core.urlresolvers import reverse
+from django.utils.translation import activate
 
 #from ..base.utils import send_email_with_db_content
 
@@ -18,10 +19,10 @@ LOGGER = logging.getLogger(__file__)
 #http://stackoverflow.com/questions/12816941/unit-testing-with-django-pipeline
 @override_settings(STATICFILES_STORAGE=
                    'pipeline.storage.NonPackagingPipelineStorage',
-                   #CELERY_EAGER_PROPAGATES_EXCEPTIONS=True,
-                   #CELERY_ALWAYS_EAGER=True,
+                   CELERY_EAGER_PROPAGATES_EXCEPTIONS=True,
+                   CELERY_ALWAYS_EAGER=True,
                    BROKER_BACKEND='memory',)
-class LandingTests(TestCase):
+class LandingTests(TransactionTestCase):
 
     fixtures = ['base_2013-05-14_final-before-beta-opening',
                 'landing_2013-05-14_final-before-beta-opening']
@@ -29,7 +30,8 @@ class LandingTests(TestCase):
     def setUp(self):
         self.client = Client()
         self.http_headers = {
-            "HTTP_ACCEPT_LANGUAGE": "fr-FR,fr;q=0.8,en-US;q=0.5,en;q=0.3",
+            #"HTTP_ACCEPT_LANGUAGE": "fr-FR,fr;q=0.8,en-US;q=0.5,en;q=0.3",
+            "HTTP_ACCEPT_LANGUAGE": "fr",
             "HTTP_USER_AGENT": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; "
             "rv:21.0) Gecko/20100101 Firefox/21.0"}
         self.test_email = 'test-ocE3f6VQqFaaAZ@1flow.io'
@@ -38,7 +40,7 @@ class LandingTests(TestCase):
         """ """
 
         response = self.client.post(
-            reverse('landing_home'), {'email': self.test_email},
+            reverse('landing_home'), {'email': ''},
             follow=True
         )
         self.assertEqual(response.status_code, 200)
@@ -52,13 +54,35 @@ class LandingTests(TestCase):
             follow=True
         )
         self.assertEqual(response.status_code, 200)
-        time.sleep(6)
+
+        self.assertEqual(mail.outbox[0].subject,
+                         'Your boarding card for the 1flow flight')
 
     def test_request_invite_lang_fr(self):
         """ This should send a mail in French. """
 
+        # cf. https://code.djangoproject.com/ticket/15143
+
+        # THIS works, but it's not what we want. Bummer!
+        activate('fr')
+
+        # THIS doesn't work, but I would have liked if it did.
+        #
+        # response = self.client.post(reverse('set_language'),
+        #                             data={'language': 'fr'},
+        #                             follow=True)
+        # self.assertEqual(response.status_code, 200)
+
+        # THIS doesn't work either, but I would have liked if it did.
+        #self.client.cookies.load({settings.LANGUAGE_COOKIE_NAME: 'fr'})
+
         response = self.client.post(
             reverse('landing_home'), {'email': self.test_email},
-            follow=True, **self.http_headers)
+            follow=True)
+
+        # THIS doesn't work neither, but I would have liked if it did.
+        # , **self.http_headers)
         self.assertEqual(response.status_code, 200)
-        time.sleep(6)
+
+        self.assertEqual(mail.outbox[0].subject,
+                         "Votre carte d'embarquement pour le vol 1flow")
