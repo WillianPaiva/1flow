6a4c410fa4a8d98ddd240f60b1c8d21b058391a7
Olivier Cortès <olive@licorn.org>
2013-06-05 15:21:26 +0200

    forgotten MAIN_SERVER, and zero get celery for free.

--- oneflow/settings/zero_1flow_io.py
+++ oneflow/settings/zero_1flow_io.py
@@ -17,6 +17,7 @@ include_snippets(
         'db_production',
         'cache_common',
         'cache_production',
+        'celery_production',
         'mail_production',
         'raven_development',
         'common_production',
