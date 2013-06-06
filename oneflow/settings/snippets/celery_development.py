a0c71dbfb13d6c0ccd9d6bc33cc4278676d9e66d
Olivier Cortès <olive@licorn.org>
2013-06-06 09:50:17 +0200

    Put the JSON serializer in celery development settings, this has helped me a lot.

--- oneflow/settings/snippets/celery_development.py
+++ oneflow/settings/snippets/celery_development.py
@@ -1,2 +1,6 @@
 BROKER_URL = 'redis://{0}:6379/1'.format(MAIN_SERVER)
 CELERY_RESULT_BACKEND = BROKER_URL
+
+# I use these to debug kombu crashes; we get a more informative message.
+#CELERY_TASK_SERIALIZER = 'json'
+#CELERY_RESULT_SERIALIZER = 'json'
