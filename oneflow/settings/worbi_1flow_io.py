608968bc10dd55a53bf99885002a9dd17a0ef99a
Olivier Cortès <olive@licorn.org>
2013-06-08 01:09:08 +0200

    More comments everywhere…

--- oneflow/settings/worbi_1flow_io.py
+++ oneflow/settings/worbi_1flow_io.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
-# Settings for obi.1flow.net (test)
+# Settings for worbi.1flow.net (preview worker) and florbi (flower instance),
+# hosted on the same machine (thus, same settings, thanks to sparks).
 
 from sparks.django.settings import clone_settings
 
 clone_settings('obi.1flow.io', __file__, globals())
-
-# worbi has no admin, it's a worker…
-FULL_ADMIN = False
