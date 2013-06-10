71c7e9b00b42e7c8a8f798e73306b8c7dfa54c1c
Olivier Cortès <olive@licorn.org>
2013-06-10 21:20:29 +0200

    Add flo{rbi,wet}.1flow.net configuration, this should avoid http://dev.1flow.net/webapps/obi1flow/group/443/ from happening.

---
+++ oneflow/settings/flower_1flow_net.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Settings for worbi.1flow.net (preview worker) and florbi (flower instance),
+# hosted on the same machine (thus, same settings, thanks to sparks).
+
+from sparks.django.settings import clone_settings
+
+clone_settings('1flow.io', __file__, globals())
