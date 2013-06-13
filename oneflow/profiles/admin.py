3057f9587dc7f17f76efcbc15f5cc17c40f46d30
Olivier Cortès <olive@licorn.org>
2013-06-13 15:27:40 +0200

    Don't allow the profile's user to be changed in the admin, even if superuser. Because this is pointless and will just make bad things happen (Spotted by Willian).

--- oneflow/profiles/admin.py
+++ oneflow/profiles/admin.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
-from django.utils.translation import ugettext_lazy as _
 
 from ..base.admin import NearlyReadOnlyAdmin, CSVAdminMixin
 from .models import UserProfile
 
 
-class UserProfileAdmin(CSVAdminMixin):
+class UserProfileAdmin(CSVAdminMixin, NearlyReadOnlyAdmin):
+    readonly_fields = ('user', )
     ordering = ('-last_modified', )
     date_hierarchy = 'last_modified'
     list_filter = ('last_modified', )
