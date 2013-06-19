1140ac167b7fd1ecae513eec305f0f95bfb89d8f
Olivier Cortès <olive@licorn.org>
2013-06-19 08:24:39 +0200

    Move REDIS_DB setting from `cache_*` to `db_*`.

--- oneflow/settings/snippets/db_preview.py
+++ oneflow/settings/snippets/db_preview.py
@@ -5,5 +5,9 @@ DATABASES['default'] = dj_database_url.config(
 
 mongoengine.connect('oneflow_test', host=MAIN_SERVER)
 
+# Redis DB is 10 to avoid clashing with production, which is 0, in case
+# both are stored on the same Redis server (which *is*, at project start).
+REDIS_DB = 10
+
 SESSION_REDIS_HOST = MAIN_SERVER
 SESSION_REDIS_DB = 12
