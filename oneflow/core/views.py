cc2bcb19b747de750990f9129ab14fe7a8e88ba6
Olivier Cortès <olive@licorn.org>
2013-06-19 11:37:12 +0200

    New version of the GR importer. Full status available via REDIS keys, updated live :-) updated JSON status view for easy (well, less difficult) ajax status updates on the client.

--- oneflow/core/views.py
+++ oneflow/core/views.py
@@ -6,7 +6,6 @@
 
 """
 
-import redis
 import logging
 import simplejson as json
 
@@ -19,15 +18,11 @@ from django.template import add_to_builtins
 from django.views.decorators.cache import never_cache
 from django.contrib.auth import authenticate, login
 
-from .models.nonrel import User as MongoUser, Subscription
 from .forms import FullUserCreationForm
 from .tasks import import_google_reader_data_trigger
+from .gr_import import GoogleReaderImport
 
 LOGGER = logging.getLogger(__name__)
-REDIS = redis.StrictRedis(host=getattr(settings, 'REDIS_FEEDBACK_HOST',
-                          'localhost'), port=6379,
-                          db=getattr(settings, 'REDIS_DB', 0))
-
 
 # Avoid the very repetitive:
 #       {% load ember js compressed i18n base_utils %}
@@ -47,8 +42,11 @@ def home(request):
     has_google = request.user.social_auth.filter(
         provider='google-oauth2').count() > 0
 
+    has_gr_import = GoogleReaderImport(request.user).running()
+
     return render(request, 'home.html', {
         'has_google': has_google,
+        'has_gr_import': has_gr_import,
     })
 
 
@@ -83,7 +81,11 @@ def import_google_reader(request):
 
     redirect_url = reverse('home') + '#/profile'
 
-    # TODO: if already_imported: return + message
+    gri = GoogleReaderImport(request.user)
+
+    if gri.running():
+        info('An import is already running for your Google Reader data.')
+        return HttpResponseRedirect(redirect_url)
 
     try:
         import_google_reader_data_trigger(request.user.id)
@@ -103,25 +105,30 @@ def import_google_reader(request):
 def import_google_reader_stats(request):
     """ A JSON View. """
 
-    user = MongoUser.objects.get(django_user=request.user.id)
+    gri = GoogleReaderImport(request.user)
 
-    if user.gr_import:
-        if user.gr_import.is_running:
-            data = {
-                'feeds': user.gr_import.feeds_imported,
-                'total': Subscription.objects(user=user),
-                'reads': user.gr_import.articles_imported,
-                'start': user.gr_import.start_time.isoformat(),
-                'done' : False,
-            }
-        else:
-            data = {
-                'done': True,
-                'start': user.gr_import.start_time.isoformat(),
-                'end': user.gr_import.end_time.isoformat(),
-            }
-    else:
+    running = gri.running()
+
+    if running is None:
         data = {'done': None}
+
+    elif running:
+        data = {
+            'feeds': gri.feeds(),
+            'total_feeds': gri.total_feeds(),
+            'articles': gri.articles(),
+            'total_articles': gri.total_articles(),
+            'start': gri.start(),
+            'done' : False,
+        }
+    else:
+        data = {
+            'done': True,
+            'start': gri.start(),
+            'end': gri.end(),
+            'total_feeds': gri.total_feeds(),
+            'total_articles': gri.total_articles(),
+        }
 
     return HttpResponse(json.dumps(data),
                         content_type="application/json")
