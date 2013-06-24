e2a5a9bdd892653cda9f5569e764c144a5a26adf
Olivier Cortès <olive@licorn.org>
2013-06-24 14:55:07 +0200

    implementation of Google reader import administration interface.

--- oneflow/core/urls.py
+++ oneflow/core/urls.py
@@ -3,9 +3,12 @@ from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
+
 
 from .views import (home, register,
                     google_reader_import,
+                    google_reader_import_stop,
                     google_reader_import_status)
 
 urlpatterns = patterns(
@@ -14,8 +17,12 @@ urlpatterns = patterns(
     url(_(r'^register/$'), register, name='register'),
     url(_(r'^signin_error/$'), TemplateView.as_view(
         template_name='signin_error.html'), name='signin_error'),
-    url(_(r'^grimport/$'), login_required(google_reader_import),
+    url(_(r'^grimport/(?:(?P<user_id>\d+)/)?$'),
+        login_required(google_reader_import),
         name='google_reader_import'),
+    url(_(r'^grstop/(?P<user_id>\d+)/$'),
+        staff_member_required(google_reader_import_stop),
+        name='google_reader_import_stop'),
     url(_(r'^grstatus/$'), login_required(google_reader_import_status),
         name='google_reader_import_status'),
 )
