1b76671dd30ef44833d4362495fbc127de5bf17d
Olivier Cortès <olive@licorn.org>
2013-07-01 20:52:58 +0200

    remove the JPype manual thing now I found it on github. But still we need to install the JDK (and not only JRE) to compile it during installation. The target is now "preinstall" because JDK must be installed / configured *before* installing JPype via PIP.

--- config/worker_medium.sh
+++ config/worker_medium.sh
@@ -5,31 +5,28 @@
 #                    role_name, env.host_string))
 
 VENV=$2
-INSTALL_FILE="~/.boilerpipe_installed"
+INSTALL_FILE="~/.io.1flow.jdk_installed"
 
 case $1 in
-    install)
+    preinstall)
 
-    if [ ! -e $INSTALL_FILE ]; then
+        if [ ! -e $INSTALL_FILE ]; then
 
-        sudo apt-get install -q --yes --force-yes openjdk-7-jre python-jpype
+            sudo apt-get install -q --yes --force-yes openjdk-7-jdk
 
-        cat >> ~/.bashrc <<EOF
+            cat >> ~/.bashrc <<EOF
 export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
 export PATH=$PATH:$JAVA_HOME
 EOF
 
-        cd ${WORKON_HOME}/${VENV}/lib/python2.7/
-        ln -sf /usr/lib/python2.7/dist-packages/jpype .
-        ln -sf /usr/lib/python2.7/dist-packages/_jpype.so .
-
-        touch ${INSTALL_FILE}
-    fi
-
-    rm JPype-*.egg-info
-
-    ln -sf /usr/lib/python2.7/dist-packages/JPype-*.egg-info .
-
-    ;;
+            touch ${INSTALL_FILE}
+        fi
+        ;;
+    install)
+        # NOT needed.
+        true
+        ;;
     *)
-
+        true
+        ;;
+esac
