81e704975dd5d5b92e9eb700a396722fb5d5484a
willian ver valen paiva <vervalenpaiva@gmail.com>
2013-07-01 11:28:09 +0200

    uupdate settings.

--- oneflow/settings/m6700.py
+++ oneflow/settings/m6700.py
@@ -5,25 +5,31 @@
 from sparks.django.settings import clone_settings
 
 clone_settings('chani', __file__, globals())
-SITE_DOMAIN = 'dev1.1flow.io'
-#SITE_DOMAIN = 'dev2.1flow.io'
-#EMAIL_HOST = 'gurney'
 
 
-#GOOGLE_CONSUMER_KEY          =     'dev1.1flow.io'
-#GOOGLE_CONSUMER_SECRET       =  'w78SFaBLwwolPWybAsAGkclr'
 
-#GOOGLE_CONSUMER_KEY          =   'dev2.1flow.io'
-#GOOGLE_CONSUMER_SECRET       =  '--EzJz4m8H2DX8ma5_YDLfIM'
+
+
+
+
+
 
 
 #Dev1
 
 GOOGLE_OAUTH2_CLIENT_ID      = '494350410052-5cgau95ek6dvhnu0c1tov8j9f10b02ou.apps.googleusercontent.com' # NOQA
 GOOGLE_OAUTH2_CLIENT_SECRET  = 'qwribFAXXLHPY4_vtR-nkpFz'
+SITE_DOMAIN = 'dev1.1flow.io'
+EMAIL_HOST = 'gurney'
+#GOOGLE_CONSUMER_KEY          =     'dev1.1flow.io'
+#GOOGLE_CONSUMER_SECRET       =  'w78SFaBLwwolPWybAsAGkclr'
 
 
 #dev2
 
+#SITE_DOMAIN = 'dev2.1flow.io'
 #GOOGLE_OAUTH2_CLIENT_ID      = '494350410052-kccc59ku23u9rs9bphh2pfbkabu1lihd.apps.googleusercontent.com' # NOQA
+#GOOGLE_CONSUMER_KEY          =   'dev2.1flow.io'
+#GOOGLE_CONSUMER_SECRET       =  '--EzJz4m8H2DX8ma5_YDLfIM'
+
 #GOOGLE_OAUTH2_CLIENT_SECRET  = 'KCjXlE9df6YiS_cZMekfh6TB'
