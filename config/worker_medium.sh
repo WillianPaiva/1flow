261f154d4fc9f3e3bd0c8b26a001bd21abfa6cb3
Olivier Cortès <olive@licorn.org>
2013-07-03 10:27:09 +0200

    more in the custom worker script.

--- config/worker_medium.sh
+++ config/worker_medium.sh
@@ -5,28 +5,28 @@
 #                    role_name, env.host_string))
 
 VENV=$2
-INSTALL_FILE="~/.io.1flow.jdk_installed"
 
 case $1 in
     preinstall)
 
-        if [ ! -e $INSTALL_FILE ]; then
+        sudo apt-get install -q --yes --force-yes openjdk-7-jdk
 
-            sudo apt-get install -q --yes --force-yes openjdk-7-jdk
+        # JAVA_HOME is not needed anymore with latest versions of JPype
+        #cat >> ~/.bashrc <<EOF
+        #export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+        #export PATH=$PATH:$JAVA_HOME
+        #EOF
 
-            # JAVA_HOME is not needed anymore with latest versions of JPype
-            #cat >> ~/.bashrc <<EOF
-            #export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
-            #export PATH=$PATH:$JAVA_HOME
-            #EOF
+        # We install jpype here, not in the requirements, to avoid
+        # installing the JDK (and fonts and many useless libraries)
+        # onto every possible 1flow machine.
+        pip install -e git+https://github.com/originell/jpype@master#egg=jpype-dev
 
-            # We install jpype here, not in the requirements, to avoid
-            # installing the JDK (and fonts and many useless libraries)
-            # onto every possible 1flow machine.
-            pip install -e git+https://github.com/originell/jpype@master#egg=jpype-dev
-
-            touch ${INSTALL_FILE}
-        fi
+        (
+            cd ${WORKON_HOME}/${VENV}/src/jpype
+            ln -sf src/python/jpype .
+            ln -sf src/python/jpypex .
+        )
         ;;
     install)
         # NOT needed.
