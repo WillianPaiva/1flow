8296f1c6e770cd45e96a88aabf740217cc42b628
Olivier Cortès <oc@1flow.io>
2013-07-24 08:37:27 +0200

    explicit email subjects for all 1flow sites

--- oneflow/settings/snippets/common_preview.py
+++ oneflow/settings/snippets/common_preview.py
@@ -6,6 +6,9 @@ TEMPLATE_CONTEXT_PROCESSORS += (
     'django.core.context_processors.debug',
 )
 
+EMAIL_SUBJECT_PREFIX='[OBI.1flow] '
+
+
 ALLOWED_HOSTS += [
     'obi.1flow.io',
     'obi.1flow.net',
