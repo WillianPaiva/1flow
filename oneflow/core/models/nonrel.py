ae018e50a177a46d437e34ba7a75d142a7541120
Olivier Cortès <oc@1flow.io>
2013-08-12 18:26:08 +0200

    Add the `Author` document. Unused yet, but code is coming, in the next patches.

--- oneflow/core/models/nonrel.py
+++ oneflow/core/models/nonrel.py
@@ -2193,6 +2193,130 @@ class Preference(Document):
     notification = EmbeddedDocumentField('NotificationPreference')
 
 
+class Author(Document):
+    name        = StringField()
+    website     = ReferenceField('WebSite', unique_with='origin_name')
+    origin_name = StringField(help_text=_(u'When trying to guess authors, we '
+                              u'have only a "fullname" equivalent. We need to '
+                              u'store it for future comparisons in case '
+                              u'firstname and lastname get manually modified '
+                              u'after author creation.'))
+
+    is_unsure = BooleanField(help_text=_(u'Set to True when the author '
+                             u'has been found from its origin_name and '
+                             u'not via email; because origin_name can '
+                             u'be duplicated, but not emails.'),
+                             default=False)
+
+    user = ReferenceField('User', required=False, help_text=_(u'Link an '
+                          u'internet author to a 1flow user.'))
+
+    duplicate_of = ReferenceField('Author')
+
+    def __unicode__(self):
+        return u'%s%s #%s for website %s' % (self.name or self.origin_name,
+                                             _(u'(unsure)')
+                                             if self.is_unsure else u'',
+                                             self.id, self.website)
+
+    def safe_reload(self):
+        try:
+            self.reload()
+
+        except:
+            pass
+
+    @classmethod
+    def get_authors_from_feedparser_article(cls, feedparser_article,
+                                            set_to_article=None):
+
+        if set_to_article:
+            # This is an absolutized URL, hopefully.
+            article_url = set_to_article.url
+
+        else:
+            article_url = getattr(feedparser_article, 'link',
+                                  '').replace(' ', '%20') or None
+
+        if article_url:
+            website = WebSite.get_from_url(article_url)
+
+        else:
+            LOGGER.critical(u'NO url, cannot get any author.')
+
+        authors = set()
+
+        if 'authors' in feedparser_article:
+            # 'authors' can be [{}], which is useless.
+
+            for author_dict in feedparser_article['authors']:
+                author = cls.get_author_from_feedparser_dict(author_dict,
+                                                             website)
+
+                if author:
+                    if set_to_article:
+                        set_to_article.update(add_to_set__authors=author)
+
+                    authors.add(author)
+
+        if 'author_detail' in feedparser_article:
+            author = cls.get_author_from_feedparser_dict(
+                feedparser_article['author_detail'], website)
+
+            if author:
+                if set_to_article:
+                    set_to_article.update(add_to_set__authors=author)
+
+                authors.add(author)
+
+        if 'author' in feedparser_article:
+            author = cls.get_author_from_feedparser_dict(
+                {'name': feedparser_article['author']}, website)
+
+            if author:
+                if set_to_article:
+                    set_to_article.update(add_to_set__authors=author)
+
+                authors.add(author)
+
+        if set_to_article and authors:
+            set_to_article.reload()
+
+        # Always return a list, else we hit
+        # http://dev.1flow.net/development/1flow-dev/group/4026/
+        return list(authors)
+
+    @classmethod
+    def get_author_from_feedparser_dict(cls, author_dict, website):
+
+        email = author_dict.get('email', None)
+
+        if email:
+            # An email is less likely to have a duplicates than
+            # a standard name. It takes precedence if it exists.
+
+            try:
+                return cls.objects.get(origin_name=email, website=website)
+
+            except cls.DoesNotExist:
+                return cls(origin_name=email, website=website,
+                           is_unsure=False).save()
+
+        origin_name = author_dict.get('name', None)
+
+        if origin_name:
+            try:
+                return cls.objects.get(origin_name=origin_name,
+                                       website=website)
+
+            except cls.DoesNotExist:
+                return cls(origin_name=origin_name,
+                           website=website,
+                           is_unsure=True).save()
+
+        return None
+
+
 def user_django_user_random_default():
     """ 20130731: unused function but I keep this code for random()-related
         and future use. """
