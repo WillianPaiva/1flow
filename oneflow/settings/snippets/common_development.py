3b1954307ee7608a6b01ce933302fe2a7dcb19bc
Olivier Cortès <oc@1flow.io>
2013-08-18 19:20:04 +0200

    Switch to flake8, pylint+pep8 are really dumb.

--- oneflow/settings/snippets/common_development.py
+++ oneflow/settings/snippets/common_development.py
@@ -21,12 +21,16 @@ PROJECT_APPS = (
 )
 
 JENKINS_TASKS = (
-    'django_jenkins.tasks.run_pylint',
     'django_jenkins.tasks.with_coverage',
     'django_jenkins.tasks.django_tests',   # select one django or
     #'django_jenkins.tasks.dir_tests'      # directory tests discovery
-    'django_jenkins.tasks.run_pep8',
-    'django_jenkins.tasks.run_pyflakes',
+    'django_jenkins.tasks.run_flake8',
+
+    # Superseded by flake8
+    #'django_jenkins.tasks.run_pep8',
+    #'django_jenkins.tasks.run_pyflakes',
+
+    #'django_jenkins.tasks.run_pylint',
     #'django_jenkins.tasks.run_jslint',
     #'django_jenkins.tasks.run_csslint',
     'django_jenkins.tasks.run_sloccount',
