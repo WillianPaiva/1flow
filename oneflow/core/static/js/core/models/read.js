b3ebc1251e6faaeda76e94aa3615192352be4be6
Olivier Cortès <oc@1flow.io>
2013-08-20 15:52:06 +0200

    Move Author and Article to their own files.

--- oneflow/core/static/js/core/models/read.js
+++ oneflow/core/static/js/core/models/read.js
@@ -1,21 +1,4 @@
 
-
-App.Author  = DS.Model.extend({
-    name: DS.attr('string')
-});
-
-App.Article = DS.Model.extend({
-    title: DS.attr('string'),
-    authors: DS.hasMany('App.Author'),
-    date_published: DS.attr('string'),
-    public_url: DS.attr('string'),
-    content: DS.attr('string'),
-    content_type: DS.attr('string'),
-    tags: DS.attr('string')
-    //feeds: DS.hasMany('App.Feed'),
-    //reads: DS.hasMany('App.Read')
-});
-
 App.Read = DS.Model.extend({
     article: DS.belongsTo('App.Article'),
     user: DS.belongsTo('App.User'),
