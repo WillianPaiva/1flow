9361f0464b00410f9df6300ef1e5c783ea01e9a3
Olivier Cortès <oc@1flow.io>
2013-08-20 10:16:17 +0200

    Fix showdown crashing if there is no markdown content.

--- oneflow/core/static/js/core/helpers/showdown.js
+++ oneflow/core/static/js/core/helpers/showdown.js
@@ -1,5 +1,7 @@
 var showdown = new Showdown.converter();
 
 Ember.Handlebars.registerBoundHelper('markdown', function(input){
-    return new Ember.Handlebars.SafeString(showdown.makeHtml(input));
+    if (input) {
+        return new Ember.Handlebars.SafeString(showdown.makeHtml(input));
+    }
 });
