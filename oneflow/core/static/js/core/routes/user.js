a17401cea270bc9072713d133146140de28da234
Olivier Cortès <oc@1flow.io>
2013-08-20 10:13:40 +0200

    Display the user profile.

--- oneflow/core/static/js/core/routes/user.js
+++ oneflow/core/static/js/core/routes/user.js
@@ -1,8 +1,8 @@
 
 App.ProfileRoute = Ember.Route.extend({
   model: function() {
-    test = App.User.find({id:window.DJANGO_JS_CONTEXT.user.id});
-    console.log(test.get('username'));
+    test = App.User.find(window.DJANGO_JS_CONTEXT.user.id);
+    //console.log(test.get('username'));
     return test;
   }
 });
