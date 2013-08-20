7137b73152e932846d25647b17a69de3efff09f3
Olivier Cortès <oc@1flow.io>
2013-08-20 15:52:44 +0200

    Implement basic infinite scrolling for reads.

--- oneflow/core/static/js/core/routes/read.js
+++ oneflow/core/static/js/core/routes/read.js
@@ -1,8 +1,22 @@
 
+// App.ReadRoute = Ember.Route.extend({
+//   model: function() {
+//     reads = App.Read.find();
+//     //console.log(reads);
+//     return reads;
+//   }
+// });
+
 App.ReadRoute = Ember.Route.extend({
   model: function() {
-    reads = App.Read.find();
-    //console.log(reads);
-    return reads;
+    return App.Read.find();
+  },
+
+  events: {
+    more: function() {
+      // NOTE: in the default configuration where the server API sends
+      // results paginated, this will automatically load next page.
+      return App.Read.find();
+    }
   }
 });
---
+++ oneflow/core/static/js/core/views/read.js
@@ -0,0 +1,31 @@
+
+// App.ReadView = Ember.View.extend(App.ShowSpinnerWhileRendering, {
+// });
+
+App.ReadView = Ember.View.extend({
+  didInsertElement: function() {
+
+    var view = this;
+    $(window).bind("scroll", function() {
+      view.didScroll();
+    });
+  },
+
+  willDestroyElement: function() {
+    $(window).unbind("scroll");
+  },
+
+  didScroll: function() {
+
+    if(this.isScrolledToBottom()) {
+      this.get('controller').send('more');
+    }
+  },
+
+  isScrolledToBottom: function() {
+    var distanceToTop = $(document).height() - $(window).height(),
+        top           = $(document).scrollTop();
+
+    return top === distanceToTop;
+  }
+});
