e89c10d567a3e1e760ee023b5fb0579043ab286c
Olivier Cortès <oc@1flow.io>
2013-08-23 11:55:53 +0200

    Add 2 more django debug toolbar panels: user-switching and mongodb. Re-organize the installed apps to be sure we don't include django-debug-toolbar-panels if it's not active in the current configuration.

--- oneflow/settings/snippets/djdt.py
+++ oneflow/settings/snippets/djdt.py
@@ -1,6 +1,11 @@
 # Debug-toolbar related
 
-INSTALLED_APPS += ('debug_toolbar', )
+INSTALLED_APPS += (
+    'debug_toolbar',
+    'debug_toolbar_user_panel',
+    'template_timings_panel',
+    'debug_toolbar_mongo',
+)
 
 MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware', )
 
@@ -13,14 +18,19 @@ INTERNAL_IPS = (
     '192.168.111.111',
 )
 
+# If it becomes too slow, activate this.
+#DEBUG_TOOLBAR_MONGO_STACKTRACES = False
+
 DEBUG_TOOLBAR_PANELS = (
     'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
     'debug_toolbar.panels.headers.HeaderDebugPanel',
     'debug_toolbar.panels.template.TemplateDebugPanel',
     'template_timings_panel.panels.TemplateTimings.TemplateTimings',
-    'debug_toolbar.panels.logger.LoggingPanel',
+    'debug_toolbar_mongo.panel.MongoDebugPanel',
     'debug_toolbar.panels.sql.SQLDebugPanel',
     'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+    'debug_toolbar_user_panel.panels.UserPanel',
     'debug_toolbar.panels.signals.SignalDebugPanel',
     'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
     'debug_toolbar.panels.version.VersionDebugPanel',
