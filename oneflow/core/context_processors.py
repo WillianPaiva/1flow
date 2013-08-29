cd974a8e254fc847bec1ed095df34ad1d99a88db
Olivier Cortès <oc@1flow.io>
2013-08-29 10:29:40 +0200

    Simplify the context processor. Not very useful anymore, in fact.

--- oneflow/core/context_processors.py
+++ oneflow/core/context_processors.py
@@ -1,20 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from .models.nonrel import User
-
 
 def mongodb_user(request):
+    """ not the most usefull context manager in the world. """
 
     if request.user.is_anonymous():
         return {u'mongodb_user': None}
 
-    try:
-        mongodb_user = User.objects.get(id=request.session[u'mongodb_user_id'])
-
-    except KeyError:
-        mongodb_user = User.objects.get(django_user=request.user.id)
-
-        # Cache it for next time.
-        request.session[u'mongodb_user_id'] = mongodb_user.id
-
-    return {u'mongodb_user': mongodb_user}
+    return {u'mongodb_user': request.user.mongo}
