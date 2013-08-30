d052f53c9b221181e690370c13ed5be92d72059c
Olivier Cortès <oc@1flow.io>
2013-08-30 10:21:51 +0200

    push the preferences and the wizards directly in template context.

--- oneflow/core/context_processors.py
+++ oneflow/core/context_processors.py
@@ -5,6 +5,14 @@ def mongodb_user(request):
     """ not the most usefull context manager in the world. """
 
     if request.user.is_anonymous():
-        return {u'mongodb_user': None}
+        return {
+            u'mongodb_user': None,
+            u'preferences': None,
+            u'wizards': None,
+        }
 
-    return {u'mongodb_user': request.user.mongo}
+    return {
+        u'mongodb_user': request.user.mongo,
+        u'preferences': request.user.mongo.preferences,
+        u'wizards': request.user.mongo.preferences.wizards,
+    }
