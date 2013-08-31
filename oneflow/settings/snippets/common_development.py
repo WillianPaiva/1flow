bd7f04c0e22e807bb09d0d04725e121dea916588
Olivier Cortès <oc@1flow.io>
2013-08-31 05:59:28 +0200

    Clean DJDT in case someone wants to use it again someday, and move useful settings to common-development.

--- oneflow/settings/snippets/common_development.py
+++ oneflow/settings/snippets/common_development.py
@@ -14,6 +14,17 @@ TEMPLATE_CONTEXT_PROCESSORS += (
 
 EMAIL_SUBJECT_PREFIX='[1flow DEV] '
 
+INTERNAL_IPS = (
+    '127.0.0.1',
+    # gurney.licorn.org
+    '109.190.93.141',
+    # my LAN address
+    '192.168.111.23',
+
+    # My LAN's proxy address, else the nginx proxy makes debug disappear.
+    '192.168.111.111',
+)
+
 PROJECT_APPS = (
     'oneflow.base',
     'oneflow.landing',
@@ -53,6 +64,8 @@ TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 JENKINS_TEST_RUNNER='django_jenkins.nose_runner.CINoseTestSuiteRunner'
 
 NOSE_ARGS = ['--stop'] #, '--ipdb', '--ipdb-failures', ]
+
+
 
 import logging
 import south.logger
