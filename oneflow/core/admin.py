3ff25afc6eb0a91db944ebcadb0ac03dc0a27974
Olivier Cortès <oc@1flow.io>
2013-09-23 21:40:41 +0200

    Use the cool Django `pgettext` to correctly translate the word `read` everywhere.

--- oneflow/core/admin.py
+++ oneflow/core/admin.py
@@ -7,7 +7,7 @@ from humanize.i18n import django_language
 from constance import config
 from django.forms import TextInput, CheckboxSelectMultiple
 from django.conf import settings
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext_lazy as _, pgettext_lazy as _p
 from django.utils.safestring import mark_safe
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
@@ -93,7 +93,7 @@ class GriOneFlowUserAdmin(UserAdmin, CSVAdminMixin):
 
         return u'%s/%s' % (number, total)
 
-    gri_reads_display.short_description = _(u'reads')
+    gri_reads_display.short_description = _p(u'noun, plural', u'reads')
 
     def gri_starred_display(self, obj):
 
