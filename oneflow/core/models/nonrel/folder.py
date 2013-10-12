9aa4acb6f3098c5f262b0350c8ac3c3996f580b9
Olivier Cortès <oc@1flow.io>
2013-10-12 18:32:18 +0200

    `Folder` enhancements: tree traversal depth limiting and external property/method changes/additions. HEADS UP: this patch is probably needed for the previous views enhancements, which I committed without looking into this file.

--- oneflow/core/models/nonrel/folder.py
+++ oneflow/core/models/nonrel/folder.py
@@ -12,7 +12,8 @@ from django.utils.translation import ugettext_lazy as _
 
 from ....base.fields import IntRedisDescriptor
 
-from .common import DocumentHelperMixin, DocumentTreeMixin, PseudoQuerySet
+from .common import (DocumentHelperMixin, DocumentTreeMixin,
+                     PseudoQuerySet, lowername)
 from .user import User
 
 LOGGER = logging.getLogger(__name__)
@@ -152,6 +153,26 @@ class Folder(Document, DocumentHelperMixin, DocumentTreeMixin):
             for child in folder_to_delete.children:
                 child.unset_parent()
 
+    @property
+    def max_depth(self):
+
+        return 5 if self.owner.preferences.selector.extended_folders_depth \
+                else 3
+
+    def get_subfolders(self, current_level):
+
+        if current_level >= self.max_depth:
+            return PseudoQuerySet()
+
+        children = PseudoQuerySet(model=self.__class__)
+
+        for child in sorted(self.children, key=lowername):
+            children.append(child)
+            children.extend(child.get_subfolders(current_level + 1))
+
+        return children
+
+
 # ————————————————————————————————————————————————————————— external properties
 #                                            Defined here to avoid import loops
 
@@ -172,16 +193,29 @@ def User_top_folders_property_get(self):
 
 
 def User_folders_tree_property_get(self):
+    """ Returns the full (but preference-via
+        depth-limited) user folders tree as a list. """
+
+    return self.get_folders_tree()
+
+
+def User_get_folders_tree_method(self, for_parent=False):
 
     folders = PseudoQuerySet(model=Folder)
 
+    # Articificialy increment the level by one to limit the folder
+    # tree to the N-1 levels. This clamps the folder manager modal.
+    level = 1 if for_parent else 0
+
     for folder in self.top_folders.order_by('name'):
         folders.append(folder)
-        folders.extend(folder.children_tree)
+        folders.extend(folder.get_subfolders(level + 1))
 
     return folders
 
-User.folders      = property(User_folders_property_get)
-User.root_folder  = property(User_root_folder_property_get)
-User.top_folders  = property(User_top_folders_property_get)
-User.folders_tree = property(User_folders_tree_property_get)
+
+User.folders          = property(User_folders_property_get)
+User.root_folder      = property(User_root_folder_property_get)
+User.top_folders      = property(User_top_folders_property_get)
+User.folders_tree     = property(User_folders_tree_property_get)
+User.get_folders_tree = User_get_folders_tree_method
