d4320db50942b107e8472036f4aeee33ca56ab65
Olivier Cortès <oc@1flow.io>
2013-10-14 10:41:46 +0200

    No-op loggers and comments.

--- oneflow/base/fields/__init__.py
+++ oneflow/base/fields/__init__.py
@@ -79,8 +79,9 @@ class RedisCachedDescriptor(object):
                                         '.', '_').replace(':', '_'))
         self.key_name = '%s%%s' % self.cache_key
 
-        # LOGGER.warning('INIT: %s > %s, cache: %s, default: %s',
-        #                self.cache_key, self.key_name, cache, default)
+        # LOGGER.warning(u'INIT: key_tmpl: %s, cache: %s, '
+        #                u'default: %s, min/max: %s/%s',
+        #                self.key_name, cache, default, min_value, max_value)
 
     def defer_callable(self, instance):
         """ This method will be run only once, at the first call of
@@ -106,6 +107,9 @@ class RedisCachedDescriptor(object):
 
     def __get__(self, instance, objtype=None):
 
+        # LOGGER.warning('GET-cache: %s %s', instance,
+        #                '_r_c_d_' + self.cache_key)
+
         if instance is None:
             raise AttributeError('Can only be accessed via an instance.')
 
@@ -118,7 +122,7 @@ class RedisCachedDescriptor(object):
                 return getattr(instance, '_r_c_d_' + self.cache_key)
 
             except AttributeError:
-                # NAH. first-time __get__?
+                # NAH. first-time __get__ on this instance.
                 key_name = '_r_c_d_' + self.cache_key
                 value    = self.__get_internal(instance, objtype)
 
@@ -143,7 +147,7 @@ class RedisCachedDescriptor(object):
 
     def __get_internal(self, instance, objtype=None):
 
-        #LOGGER.warning('GET-redis: %s', self.key_name % instance.id)
+        # LOGGER.warning('GET-redis: %s', self.key_name % instance.id)
 
         if self.default is None:
 
@@ -212,7 +216,9 @@ class RedisCachedDescriptor(object):
 
     def __set__(self, instance, value):
 
-        #LOGGER.warning('SET-redis: %s %s', self.key_name % instance.id, value)
+        # LOGGER.warning('SET-redis: %s %s, min/max clamps: %s/%s',
+        #                self.key_name % instance.id, value,
+        #                self.min_value, self.max_value)
 
         if self.min_value is not None and value < self.min_value:
             value = self.min_value
@@ -232,7 +238,7 @@ class RedisCachedDescriptor(object):
 
     def __delete__(self, instance):
 
-        #LOGGER.warning('DELETE-redis: %s', self.key_name % instance.id)
+        # LOGGER.warning('DELETE-redis: %s', self.key_name % instance.id)
 
         self.REDIS.delete(self.key_name % instance.id)
 
@@ -253,6 +259,20 @@ class IntRedisDescriptor(RedisCachedDescriptor):
         See :class:`RedisCachedDescriptor` for generic descriptor information
         (like callable default values and network-wide locking semantics).
     """
+
+    #
+    # TODO: implement http://stackoverflow.com/a/11988086/654755
+    #
+    # Instead of
+    #       return int(val)
+    #
+    # we should
+    #       return IntRedisCachedDescriptorProxy(val)
+    #
+    # for all integer related operations to work. This will probably be
+    # trickier to do on more complex-typed descriptors. Or not.
+    #
+    # The ListRedisProxy was a started attempt.
 
     def to_python(self, value):
 
