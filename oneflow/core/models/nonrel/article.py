4e53827b0837a892704481b32e29e51dcf3e4947
Olivier Cortès <oc@1flow.io>
2013-10-16 13:32:34 +0200

    Add a new `Read.is_good` attribute (not a property), following `Article.is_good` (dynamic) property. Now an article will "activate()" its reads when it is "good". This avoid false-positive in counters and user deception/frustration when they see "N unread" but only <N is available because of duplicates/orphaned/unparsed/etc.

--- oneflow/core/models/nonrel/article.py
+++ oneflow/core/models/nonrel/article.py
@@ -957,6 +957,8 @@ class Article(Document, DocumentHelperMixin):
             LOGGER.exception(u'Extraction failed for article %s.', self)
             return
 
+        self.activate_reads()
+
     @property
     def is_good(self):
         """ Return ``True`` if the current article
@@ -979,6 +981,19 @@ class Article(Document, DocumentHelperMixin):
             return False
 
         return True
+
+    def activate_reads(self, force=False, verbose=False):
+
+        if self.is_good or force:
+            for read in self.bad_reads:
+                # We 'force' to avoid the reverse test in Read.
+                # We already know the article is OK.
+                read.activate(force=True)
+
+        else:
+            if verbose:
+                LOGGER.warning(u'Will not activate reads of bad article %s',
+                               self)
 
     def find_image_must_abort(self, force=False, commit=True):
 
