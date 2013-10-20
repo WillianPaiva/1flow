c4b399a5e4187d84e8330094154476e0207fab3f
Olivier Cortès <oc@1flow.io>
2013-10-21 00:12:43 +0200

    Add a label and some help-text, which miss made them untranslated in the interface.

--- oneflow/core/forms.py
+++ oneflow/core/forms.py
@@ -175,8 +175,12 @@ class ManageFolderForm(DocumentForm):
                                  required=False, widget=Select2Widget())
 
     subscriptions = OnlyNameMultipleChoiceField(
-        queryset=Subscription.objects.none(), required=False,
-        widget=Select2MultipleWidget())
+        label=_(u'Subscriptions'), queryset=Subscription.objects.none(),
+        required=False, widget=Select2MultipleWidget(),
+        help_text=_(u'These are the ones held directly by the folder; they '
+                    u'are displayed above subfolders. There can be none, if '
+                    u'you prefer dispatching your subscriptions in subfolders '
+                    u'only.'))
 
     class Meta:
         model = Folder
