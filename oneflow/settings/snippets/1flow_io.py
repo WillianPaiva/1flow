dc040929adb72b9c9fd855a3edc10f0d452b7964
Olivier Cortès <oc@1flow.io>
2013-10-30 00:30:52 +0100

    Full admin in production, we need to catch things like http://1flow.userecho.com/users/534599-emmanuel-domingos/feedback/ FAST.

--- oneflow/settings/snippets/1flow_io.py
+++ oneflow/settings/snippets/1flow_io.py
@@ -8,3 +8,8 @@ SITE_NAME   = '1flow'
 # WARNING: keep this a date(), which is neither naive nor TZ aware.
 LANDING_BETA_DATE = datetime.date(2013, 07, 01)
 LANDING_BETA_INVITES = 100
+
+# We now need full access to content editors in production for fast fixes.
+# This is not definitive, but will help making content more user-friendly
+# without hassling back-and-forth between development and production via CLI.
+FULL_ADMIN = True
