56914de85107763aa682917e54317730ae18eae4
Olivier Cortès <oc@1flow.io>
2014-01-20 09:04:41 +0100

    Insert copyrights and license requisites for the upcoming public release. Create a basic COPYING file explaining what is libre and what is not, until we work a solution.

--- oneflow/settings/snippets/common.py
+++ oneflow/settings/snippets/common.py
@@ -1,9 +1,30 @@
 # -*- coding: utf-8 -*-
+#
 # Django common settings for oneflow project.
 # These are overridden in hostname.py specific files.
 #
 # Here we define the settings for the test platform.
 # Production will override only the needed directives.
+#
+"""
+    Copyright 2013 Olivier Cortès <oc@1flow.io>
+
+    This file is part of the 1flow project.
+
+    1flow is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of
+    the License, or (at your option) any later version.
+
+    1flow is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public
+    License along with 1flow.  If not, see http://www.gnu.org/licenses/
+
+"""
 
 import os
 import sys
@@ -274,6 +295,7 @@ PIPELINE_JS = {
     'utils': {
         'source_filenames': (
             'js/utils/*.js',
+            'js/common.js',
         ),
         'output_filename': 'js/min/utils.js',
     },
--- oneflow/settings/snippets/common_development.py
+++ oneflow/settings/snippets/common_development.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #
 # Include your development machines hostnames here.
 #
@@ -7,6 +8,26 @@
 # here.
 #
 # If you connect via http://localhost:8000/, everything is already OK.
+#
+"""
+    Copyright 2013 Olivier Cortès <oc@1flow.io>
+
+    This file is part of the 1flow project.
+
+    1flow is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of
+    the License, or (at your option) any later version.
+
+    1flow is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public
+    License along with 1flow.  If not, see http://www.gnu.org/licenses/
+
+"""
 
 TEMPLATE_CONTEXT_PROCESSORS += (
     'django.core.context_processors.debug',
