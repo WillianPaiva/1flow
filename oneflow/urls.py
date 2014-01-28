2beabafa7afa8a2dd2763386e746ad4f778833b6
Olivier Cortès <oc@1flow.io>
2014-01-28 21:10:20 +0100

    project and core URLs updated for django-{inplaceedit,user-accounts}. INCOMPLETE PATCH.

--- oneflow/urls.py
+++ oneflow/urls.py
@@ -27,6 +27,7 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
 from django.views.generic import TemplateView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.utils.translation import ugettext_lazy as _
 
 from .api import v1_api
 
@@ -57,6 +58,7 @@ urlpatterns = patterns(
         content_type='text/plain')),
     url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^djangojs/', include('djangojs.urls')),
+    url(r'^inplaceeditform/', include('inplaceeditform.urls')),
 
     #
     # HEADS UP: this URL belongs to `core` and is not translated.
@@ -86,6 +88,60 @@ else:
         url(r'^$', root_redirects_to_home, name='core_root'),
     )
 
+
+if 'account' in settings.INSTALLED_APPS:
+    # We use localized URLs in our app. django-user-accounts doesn't. Too bad.
+
+    from account.views import (SignupView, LoginView, LogoutView, DeleteView,
+                               ConfirmEmailView, ChangePasswordView,
+                               PasswordResetView, PasswordResetTokenView,
+                               SettingsView)
+
+    urlpatterns += i18n_patterns(
+        "",
+        url(_(r"^signup/$"), SignupView.as_view(),
+            name="account_signup"),
+        # original is: url(_(r"^login/$"), but we prefer:
+        url(_(ur'^signin/$'), LoginView.as_view(),
+            name="signin"),
+        # original is: url(_(r"^logout/$"), but we prefer:
+        url(_(ur'^signout/$'), LogoutView.as_view(),
+            name="signout"),
+        url(_(r"^confirm_email/(?P<key>\w+)/$"), ConfirmEmailView.as_view(),
+            name="account_confirm_email"),
+        url(_(r"^password/$"), ChangePasswordView.as_view(),
+            name="account_password"),
+        url(_(r"^password/reset/$"), PasswordResetView.as_view(),
+            name="account_password_reset"),
+        url(_(r"^password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$"),
+            PasswordResetTokenView.as_view(),
+            name="account_password_reset_token"),
+        url(_(r"^settings/$"), SettingsView.as_view(),
+            name="account_settings"),
+        url(_(r"^delete/$"), DeleteView.as_view(),
+            name="account_delete"),
+    )
+
+    # As an alternative, we could have used one indirection level,
+    # but it's not satisfying because of the non-i18n nature of
+    # account's URLs.
+    #
+    # lambda r: HttpResponseRedirect(reverse_lazy('account_login')),
+    # name='signin'),
+    # lambda r: HttpResponseRedirect(reverse_lazy('account_logout')),
+    # name='signout'),
+
+else:
+    # In case we do not have the django-user-accounts application,
+    # We can still provide basic templates and features, notably
+    # all social-related ones.
+    urlpatterns += patterns(
+        'django.contrib.auth.views',
+        url(_(ur'^signin/$'), 'login',
+            {'template_name': 'account/login.html'}, name='signin'),
+        url(_(ur'^signout/$'), 'logout',
+            {'template_name': 'account/logout.html'}, name='signout'),
+    )
 
 urlpatterns += i18n_patterns(
     '',
