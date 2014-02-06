1b9f8b9d85997e9563002f2164f14fc1eb86ab7c
Olivier Cortès <oc@1flow.io>
2014-02-06 12:13:38 +0100

    Add the new pipeline step (still, copy-n-paste of a non cherry-picked patch).

--- oneflow/settings/snippets/common.py
+++ oneflow/settings/snippets/common.py
@@ -617,6 +617,11 @@ SOCIAL_AUTH_PIPELINE = (
 
     #'oneflow.core.social_pipeline.check_1flow_requirements',
     'oneflow.core.social_pipeline.get_social_avatar',
+
+    # Given the current configuration, we do allow any user to register,
+    # or not. In both cases, we create the account, but will deactivate
+    # it if registration is disabled.
+    'oneflow.core.social_pipeline.throttle_new_user_accounts',
 )
 
 # —————————————————————————————————————————————————————————————— 1flow settings
