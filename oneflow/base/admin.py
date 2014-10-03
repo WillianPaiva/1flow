eeadce1b0e9adc009a7842e1a03f04033c5e509d
Olivier Cortès <oc@1flow.io>
2014-10-04 00:17:42 +0200

    update to sparks latest (develop).

--- oneflow/base/admin.py
+++ oneflow/base/admin.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
 """
-    Copyright 2012-2014 Olivier Cortès <oc@1flow.io>
+Copyright 2012-2014 Olivier Cortès <oc@1flow.io>.
 
-    This file is part of the 1flow project.
+This file is part of the 1flow project.
 
-    1flow is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as
-    published by the Free Software Foundation, either version 3 of
-    the License, or (at your option) any later version.
+1flow is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
 
-    1flow is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
+1flow is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
 
-    You should have received a copy of the GNU Affero General Public
-    License along with 1flow.  If not, see http://www.gnu.org/licenses/
+You should have received a copy of the GNU Affero General Public
+License along with 1flow.  If not, see http://www.gnu.org/licenses/
 
 """
 
@@ -35,11 +35,10 @@ from django.utils.datastructures import SortedDict
 
 
 from django.contrib import admin
-#import mongoadmin as admin
 
 from .models import EmailContent, User
 
-from sparks.django.admin import languages, truncate_field
+from sparks.django.utils import languages, truncate_field
 
 
 LOGGER = logging.getLogger(__name__)
