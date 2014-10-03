2a661a099f85b3b2f700e6538c08360bf51e707f
Olivier Cortès <oc@1flow.io>
2014-10-04 01:18:12 +0200

    pep257 happyness.

--- oneflow/core/stats.py
+++ oneflow/core/stats.py
@@ -1,22 +1,21 @@
 # -*- coding: utf-8 -*-
 """
-    Copyright 2013-2014 Olivier Cortès <oc@1flow.io>
+Copyright 2013-2014 Olivier Cortès <oc@1flow.io>.
 
-    This file is part of the 1flow project.
+This file is part of the 1flow project.
 
-    1flow is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as
-    published by the Free Software Foundation, either version 3 of
-    the License, or (at your option) any later version.
+1flow is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
 
-    1flow is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
+1flow is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
 
-    You should have received a copy of the GNU Affero General Public
-    License along with 1flow.  If not, see http://www.gnu.org/licenses/
-
+You should have received a copy of the GNU Affero General Public
+License along with 1flow.  If not, see http://www.gnu.org/licenses/
 """
 
 import logging
@@ -24,8 +23,6 @@ import operator
 
 from statsd import statsd
 from constance import config
-#from mongoengine.queryset import Q
-#from mongoengine.context_managers import no_dereference
 
 from celery import task
 
@@ -534,6 +531,8 @@ def synchronize_statsd_authors_gauges(full=False):
 
 @task(queue='low')
 def synchronize_statsd_gauges(full=False):
+    """ Synchronize all counters to statsd. """
+
     synchronize_statsd_articles_gauges(full=full)
     synchronize_statsd_tags_gauges(full=full)
     synchronize_statsd_websites_gauges(full=full)
