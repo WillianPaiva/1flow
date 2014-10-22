771c4e99aca074faa6d79e857ff5a913a56929b2
Olivier Cortès <oc@1flow.io>
2014-10-22 10:09:18 +0200

    move mongo tasks to a dedicated file, to ease transition (variable renames).

--- oneflow/core/tasks/mongo.py
+++ oneflow/core/tasks/mongo.py
@@ -19,7 +19,6 @@ License along with 1flow.  If not, see http://www.gnu.org/licenses/
 """
 
 import logging
-import time as pytime
 
 from random import randrange
 from constance import config
@@ -28,57 +27,36 @@ from celery import task
 
 from django.utils.translation import ugettext_lazy as _
 
-from ..models.reldb import (
-    MailAccount,
-    BaseFeed, basefeed_refresh_task
-)
-
 from oneflow.base.utils import RedisExpiringLock
-from oneflow.base.utils.dateutils import (now, today, timedelta,
+from oneflow.base.utils.dateutils import (now, timedelta,
                                           naturaldelta, benchmark)
 
-from gr_import import clean_gri_keys
-
-from mongo import refresh_all_mongo_feeds
-
-# Import this one, so that celery can find it,
-# Else it complains about a missing import.
-from checks import global_checker_task  # NOQA
+from ..models.nonrel import (
+    Feed as MongoFeed, feed_refresh_task as mongo_feed_refresh_task
+)
 
 LOGGER = logging.getLogger(__name__)
 
 
-@task(queue='clean')
-def clean_obsolete_redis_keys():
-    """ Call in turn all redis-related cleaners. """
-
-    start_time = pytime.time()
-
-    if today() <= (config.GR_END_DATE + timedelta(days=1)):
-        clean_gri_keys()
-
-    LOGGER.info(u'clean_obsolete_redis_keys(): finished in %s.',
-                naturaldelta(pytime.time() - start_time))
-
-
 @task(queue='high')
-def refresh_all_feeds(limit=None, force=False):
-    """ Refresh all feeds (RSS/Mail/Twitter…). """
+def refresh_all_mongo_feeds(limit=None, force=False):
+    """ Refresh all MongoEngine feeds (RSS).
+
+    .. note:: this task should vanish when
+        MongoDB → PostgreSQL migration is done.
+    """
 
     if config.FEED_FETCH_DISABLED:
         # Do not raise any .retry(), this is a scheduled task.
         LOGGER.warning(u'Feed refresh disabled in configuration.')
         return
 
-    # TODO: WIPE THIS when Mongo → PG migration is complete!
-    refresh_all_mongo_feeds.delay()
-
     # Be sure two refresh operations don't overlap, but don't hold the
     # lock too long if something goes wrong. In production conditions
     # as of 20130812, refreshing all feeds takes only a moment:
     # [2013-08-12 09:07:02,028: INFO/MainProcess] Task
     #       oneflow.core.tasks.refresh_all_feeds succeeded in 1.99886608124s.
-    my_lock = RedisExpiringLock('refresh_all_feeds', expire_time=120)
+    my_lock = RedisExpiringLock('refresh_all_mongo_feeds', expire_time=120)
 
     if not my_lock.acquire():
         if force:
@@ -93,13 +71,15 @@ def refresh_all_feeds(limit=None, force=False):
             LOGGER.warning(u'refresh_all_feeds() is already locked, aborting.')
             return
 
-    # This should bring us a Polymorphic Query to refresh all feeds types.
-    feeds = BaseFeed.objects.filter(is_active=True, is_internal=False)
+    feeds = MongoFeed.objects.filter(closed__ne=True, is_internal__ne=True)
 
     if limit:
         feeds = feeds.limit(limit)
 
-    with benchmark('refresh_all_feeds()'):
+    # No need for caching and cluttering CPU/memory for a one-shot thing.
+    feeds.no_cache()
+
+    with benchmark('refresh_all_mongo_feeds()'):
 
         try:
             count = 0
@@ -115,7 +95,7 @@ def refresh_all_feeds(limit=None, force=False):
 
                 if feed.last_fetch is None:
 
-                    basefeed_refresh_task.delay(feed.id)
+                    mongo_feed_refresh_task.delay(feed.id)
 
                     LOGGER.info(u'Launched immediate refresh of feed %s which '
                                 u'has never been refreshed.', feed)
@@ -129,12 +109,12 @@ def refresh_all_feeds(limit=None, force=False):
                         countdown = randrange(
                             config.FEED_REFRESH_RANDOMIZE_DELAY)
 
-                        basefeed_refresh_task.apply_async((feed.id, force),
-                                                          countdown=countdown)
+                        mongo_feed_refresh_task.apply_async((feed.id, force),
+                                                            countdown=countdown)
 
                     else:
                         countdown = 0
-                        basefeed_refresh_task.delay(feed.id, force)
+                        mongo_feed_refresh_task.delay(feed.id, force)
 
                     LOGGER.info(u'%s refresh of feed %s %s (%s late).',
                                 u'Scheduled randomized'
@@ -150,66 +130,3 @@ def refresh_all_feeds(limit=None, force=False):
 
         LOGGER.info(u'Launched %s refreshes out of %s feed(s) checked.',
                     count, feeds.count())
-
-
-@task(queue='high')
-def refresh_all_mailaccounts(force=False):
-    """ Check all unusable e-mail accounts. """
-
-    if config.MAIL_ACCOUNT_REFRESH_DISABLED:
-        # Do not raise any .retry(), this is a scheduled task.
-        LOGGER.warning(u'E-mail accounts check disabled in configuration.')
-        return
-
-    accounts = MailAccount.objects.filter(is_usable=False)
-
-    my_lock = RedisExpiringLock('check_email_accounts',
-                                expire_time=30 * (accounts.count() + 2))
-
-    if not my_lock.acquire():
-        if force:
-            my_lock.release()
-            my_lock.acquire()
-            LOGGER.warning(_(u'Forcing check of email accounts…'))
-
-        else:
-            # Avoid running this task over and over again in the queue
-            # if the previous instance did not yet terminate. Happens
-            # when scheduled task runs too quickly.
-            LOGGER.warning(u'refresh_all_mailaccounts() is already locked, '
-                           u'aborting.')
-            return
-
-    with benchmark('refresh_all_mailaccounts()'):
-
-        try:
-            for account in accounts:
-                try:
-                    account.test_connection()
-                    account.update_mailboxes()
-
-                except:
-                    pass
-
-        finally:
-            my_lock.release()
-
-        LOGGER.info(u'Launched %s checks on unusable accounts out of %s total.',
-                    accounts.count(), MailAccount.objects.all().count())
-
-
-@task(queue='low')
-def synchronize_statsd_gauges(full=False):
-    """ Synchronize all counters to statsd. """
-
-    from oneflow.core.stats import (
-        synchronize_statsd_articles_gauges,
-        synchronize_statsd_tags_gauges,
-        synchronize_statsd_websites_gauges,
-        synchronize_statsd_authors_gauges,
-    )
-
-    synchronize_statsd_articles_gauges(full=full)
-    synchronize_statsd_tags_gauges(full=full)
-    synchronize_statsd_websites_gauges(full=full)
-    synchronize_statsd_authors_gauges(full=full)
