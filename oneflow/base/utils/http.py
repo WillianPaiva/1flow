32326f72cc7074851dd60d8134a6fd43fce25d12
Olivier Cortès <oc@1flow.io>
2014-11-11 22:12:35 +0100

    URL love: move split_url() and the feedsportal cleaner to base.utils.http. Make the feedsportal cleaner lighter and not depend on the content.

--- oneflow/base/utils/http.py
+++ oneflow/base/utils/http.py
@@ -20,6 +20,8 @@ License along with 1flow.  If not, see http://www.gnu.org/licenses/
 
 import re
 
+from collections import namedtuple
+
 UTM_RE    = re.compile(ur'[&]?utm_[^=]+(=[^&]*)?', re.I)
 XTOR_RE   = re.compile(ur'[&#]?xtor(=[^&]*)?', re.I)
 EQL_RE    = re.compile(ur'[#&?]=[^&]+', re.I)
@@ -29,6 +31,72 @@ GGL_RE    = re.compile(ur'^[htps]+://[\w.]*google.com/(?:news/)?'
                        ur'url\?[\w=&]+url=([^&]+)[\w=&]*', re.I)
 
 __all__ = ('clean_url', 'clean_google_url', 'clean_marketing_url', )
+
+
+url_tuple = namedtuple('url', ['scheme', 'host_and_port', 'remaining', ])
+url_port_tuple = namedtuple('url_port',
+                            ['scheme', 'hostname', 'port', 'remaining', ])
+
+FEEDSPORTAL_REPLACEMENTS = {
+    u'A': u'0',
+    u'B': u'.',
+    u'C': u'/',
+    u'D': u'?',
+    u'E': u'-',
+    u'I': u'_',
+    u'L': u'http://',
+    u'S': u'www.',
+    u'N': u'.com',
+    u'O': u'.co.uk',
+}
+
+
+class SplitUrlException(Exception):
+
+    """ Raised when an URL is reaaaaally bad. """
+
+    pass
+
+
+def split_url(url, split_port=False):
+    u""" Split an URL into a named tuple for easy manipulations.
+
+    Eg. “http://test.com/toto becomes:
+    ('scheme'='http', 'host_and_port'='test.com', 'remaining'='toto').
+
+    if :param:`split_port` is ``True``, the returned namedtuple is of the form:
+
+    ('scheme'='http', 'hostname'='test.com', 'port'=80, 'remaining'='toto').
+
+    In this case, ``port`` will be an integer. All other attributes are strings.
+
+    In case of an error, it will raise a :class:`SplitUrlException` exception.
+    """
+
+    try:
+        proto, remaining = url.split('://', 1)
+
+    except:
+        raise SplitUrlException(u'Unparsable url “{0}”'.format(url))
+
+    try:
+        host_and_port, remaining = remaining.split('/', 1)
+
+    except ValueError:
+        host_and_port = remaining
+        remaining     = u''
+
+    if split_port:
+        try:
+            hostname, port = host_and_port.split(':')
+
+        except ValueError:
+            hostname = host_and_port
+            port = '80' if proto == 'http' else '443'
+
+        return url_port_tuple(proto, hostname, int(port), remaining)
+
+    return url_tuple(proto, host_and_port, remaining)
 
 
 def clean_google_url(url):
@@ -53,7 +121,7 @@ def clean_marketing_url(url):
             u'', EQL_RE.sub(
                 u'', UTM_RE.sub(
                     u'', XTOR_RE.sub(
-                        u'', url
+                        u'', url.replace('?source=ehesitrf0000001', '')
                     )
                 )
             )
@@ -61,9 +129,38 @@ def clean_marketing_url(url):
     )
 
 
+def clean_feedsportal_url(url):
+    """ Get RID of those damn feedsportal shitty URLs. """
+
+    try:
+        url_start = url[:32]
+
+    except:
+        # the URL is definitely too short to contain a feedsportal thing.
+        return url
+
+    if '.feedsportal.com' in url_start:
+        # Cf. http://www.pictuga.com/fiche-1125.html
+
+        match = re.search('/([0-9a-zA-Z]{20,})/\w+\.htm$', url)
+
+        if match:
+            url = match.groups()[0].split('0')
+
+            return u''.join([
+                (FEEDSPORTAL_REPLACEMENTS[s[0]]
+                 if s[0] in FEEDSPORTAL_REPLACEMENTS
+                 else u'=') + s[1:] for s in url[1:]
+            ])
+
+
 def clean_url(url):
     """ Chain all URL cleaning functions. Expensive, but worth the result. """
 
     return clean_marketing_url(
-        clean_google_url(url)
+        clean_google_url(
+            clean_feedsportal_url(
+                url
+            )
+        )
     )
