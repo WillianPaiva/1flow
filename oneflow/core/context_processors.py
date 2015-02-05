3e90b81af2690623fa23ad7fd2fe0e774186a1f5
Olivier Cortès <oc@1flow.io>
2015-02-05 12:52:09 +0100

    flake8 and inject ORIGINS into the template context.

--- oneflow/core/context_processors.py
+++ oneflow/core/context_processors.py
@@ -1,33 +1,27 @@
 # -*- coding: utf-8 -*-
-"""
-    Copyright 2013-2014 Olivier Cortès <oc@1flow.io>
+u"""
+Copyright 2013-2014 Olivier Cortès <oc@1flow.io>.
 
-    This file is part of the 1flow project.
+This file is part of the 1flow project.
 
-    1flow is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as
-    published by the Free Software Foundation, either version 3 of
-    the License, or (at your option) any later version.
+1flow is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
 
-    1flow is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
+1flow is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
 
-    You should have received a copy of the GNU Affero General Public
-    License along with 1flow.  If not, see http://www.gnu.org/licenses/
+You should have received a copy of the GNU Affero General Public
+License along with 1flow.  If not, see http://www.gnu.org/licenses/
 
 """
 
 from django.conf import settings
-from models.nonrel import (
-    CONTENT_TYPES,
-    CONTENT_TYPES_FINAL,
-    CACHE_ONE_HOUR, CACHE_ONE_DAY,
-    CACHE_ONE_WEEK, CACHE_ONE_MONTH,
-)
 
-from models.reldb import IMPORT_STATUS
+from oneflow.core import models
 
 
 def mongodb_user(request):
@@ -57,23 +51,29 @@ def mongodb_user(request):
 
 
 def models_constants(request):
-    """ Inject content types into the context, so we can use them
-        by name instead of hardcoding their integer values. """
+    """ Inject content types into the context.
+
+    So we can use them by name instead of hardcoding their integer values.
+
+    Best used with sparks's lookup() template tag.
+    """
 
     return {
-        u'CONTENT_TYPES':     CONTENT_TYPES,
-        u'CONTENT_TYPES_FINAL':   CONTENT_TYPES_FINAL,
+        u'ORIGINS': models.ORIGINS,
+        u'CONTENT_TYPES':     models.CONTENT_TYPES,
+        u'CONTENT_TYPES_FINAL':   models.CONTENT_TYPES_FINAL,
 
-        u'IMPORT_STATUS': IMPORT_STATUS,
+        u'IMPORT_STATUS': models.IMPORT_STATUS,
 
-        u'CACHE_ONE_HOUR':        CACHE_ONE_HOUR,
-        u'CACHE_ONE_DAY':         CACHE_ONE_DAY,
-        u'CACHE_ONE_WEEK':        CACHE_ONE_WEEK,
-        u'CACHE_ONE_MONTH':       CACHE_ONE_MONTH,
+        u'CACHE_ONE_HOUR':        models.CACHE_ONE_HOUR,
+        u'CACHE_ONE_DAY':         models.CACHE_ONE_DAY,
+        u'CACHE_ONE_WEEK':        models.CACHE_ONE_WEEK,
+        u'CACHE_ONE_MONTH':       models.CACHE_ONE_MONTH,
     }
 
 
 def social_things(request):
+    """ Put django social auth 1flow's specifics in the context. """
 
     if request.user.is_anonymous():
         return {}
